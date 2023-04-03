851e63284b4d59ef20cf52bef7301e4611f9336a
Junaid Shah <shah@galaxyitc.com>
2023-04-03 22:31:21 +0500

    added new change

--- custom_company_logo/__manifest__.py
+++ custom_company_logo/__manifest__.py
@@ -6,10 +6,11 @@
     'category': 'Extra Tools',
     'author': 'Junaid Shah',
     'website': 'www.odooindex.com',
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'account'],
     'data': [
         'views/external_layout.xml',
         'views/res_company_view.xml',
+        'views/invoice_inherit.xml',
     ],
     'installable': True,
     'auto_install': False,
